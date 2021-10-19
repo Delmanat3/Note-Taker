a2d5f13324c2d428c59a6c607fd87f337a20dcf6
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-10-19 18:06:16 -0500

    still thinking im doing something wrong

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,12 @@ const fs = require("fs");
 const path = require("path");
 const app = require("express").Router();
 
+const rando=require('../Цікавий')
+
 app.GET('/notes', (req,res)=>{
     console.log(`${req.method} request received so calm your pretty little ass down `)
     console.log('//////be patient you lazy fuck////////')
-    return res.status(200).json(db);
+    res.status(200).json(db);
 
 }
 )

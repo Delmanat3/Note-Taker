a5500d57708ebcb2e612a28a31287cd7cfc8ed02
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-10-19 17:55:43 -0500

    my overconfidence remains...

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,15 @@
-const router = require("express").Router();
+
 const db = require("../db/db");
 const fs = require("fs");
 const path = require("path");
+const app = require("express").Router();
 
+app.GET('/notes', (req,res)=>{
+    console.log(`${req.method} request received so calm your pretty little ass down `)
+    console.log('//////be patient you lazy fuck////////')
+    return res.status(200).json(db);
 
+}
+)
 
 module.exports = router;

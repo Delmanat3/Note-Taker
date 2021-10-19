a123939a993201aa00d353d92ce90cad4dcc632f
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-10-19 18:24:37 -0500

    fr dude this seems wrong

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const app = require("express").Router();
 
-const rando=require('../Цікавий')
+const rando = require('../Цікавий')
 
 app.GET('/notes', (req,res)=>{
     console.log(`${req.method} request received so calm your pretty little ass down `)
@@ -14,4 +14,52 @@ app.GET('/notes', (req,res)=>{
 }
 )
 
-module.exports = router;
+
+app.post('/notes',(req,res)=>{
+    console.log('be warned \n your pathetic attempt has been noticed')
+    //if(req.body && req.title){
+
+   // }
+    let fileyBoy = path.join(__dirname, "../db/db.json");
+
+    let sirNote = req.body
+
+    sirNote.id = rando()
+
+    console.log(`Your note has it's own designation.... isn't that cute. here it is:${sirNote.id}`)
+
+    db.push(sirNote)
+
+    fs.writeFile(fileyBoy, JSON.stringify(db), function (err) {
+        err
+          ? console.log(err)
+          : console.log('fortune favored you this time boy.');
+      });
+    
+      
+      res.json(note);
+})
+
+
+// app.post('/api/reviews', (req, res) => {
+//     // Log that a POST request was received
+//     console.info(`${req.method} request received to add a review`);
+  
+//     // Prepare a response object to send back to the client
+//     let response;
+  
+//     // Check if there is anything in the response body
+//     if (req.body && req.body.product) {
+//       response = {
+//         status: 'success',
+//         data: req.body,
+//       };
+//       res.status(201).json(response);
+//     } else {
+//       res.status(400).json('Request body must at least contain a product name');
+//     }
+  
+//     // Log the response body to the console
+//     console.log(req.body);
+//   });
+module.exports = app;

e05e25dfc8a395a71abf313f47a72be20c87b818
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-10-19 19:08:40 -0500

    stringify has betrayed me

--- routes/api.js
+++ routes/api.js
@@ -14,10 +14,10 @@ app.get('/notes', (req,res)=>{
 }
 )
 
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
+// const writeToFile = (destination, content) =>
+//   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+//     err ? console.error(err) : console.info(`\nData written to ${destination}`)
+//   );
 const readAndAppend = (content , file) => {
     fs.readFile(file, 'utf8', (err, data) => {
       if (err) {
